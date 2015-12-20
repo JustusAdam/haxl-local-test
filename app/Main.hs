d1c85b8bdd42120e09c8e82eee937215e0b568cc
Justus Adam <dev@justus.science>
2015-12-20 15:46:04 +0100

    Added some comments to explain the code.

--- app/Main.hs
+++ app/Main.hs
@@ -12,19 +12,28 @@ singleton a = [a]
 main :: IO ()
 main = do
   myEnv <- initEnv
-    (stateSet MovieReqState $ stateSet StoreReqState stateEmpty) StoreReqState
+             (stateSet MovieReqState $
+               stateSet StoreReqState stateEmpty)
+             StoreReqState
   data' <- runHaxl myEnv $
     (++)
-      <$> sequenceA
+      <$> sequenceA -- All top-level fetches here will be batched in the first
+                    -- round because sequenceA uses Applicative
         [ getActor "Harry Potter"
         , getActor "Ron Weasley"
         , getActorForMovie "Inception"
-        , getActor "Argus Filtch" >>= maybe (getActor "Minerva McGonagall") (return . Just)
+        , getActor "Argus Filtch" >>=
+            maybe -- `maybe` does a pattern match to create a data dependency and
+                  -- push the subsequent request to the second round
+              (getActor "Minerva McGonagall")
+              (return . Just)
         ]
       <*> do
-        h <- getActor "Harry Potter"
+        h <- getActor "Harry Potter" -- this will also land in the first round
+                                     -- because it is the topmost fetch in the
+                                     -- do block
         let ron = getActor "Ron Weasley"
-        case h of
+        case h of -- and a pattern match again to push `ron` to the second round
           Nothing -> singleton <$> ron
           Just _ -> (: [h]) <$> ron
 
